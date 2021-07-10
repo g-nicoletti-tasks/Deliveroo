20208107a0217e1da05a5eedf5906eb776b8790f
Giuseppe Nicoletti <giu.nicoletti@gmail.com>
2021-07-10 23:21:36 +0100

    skimr test

--- analysis.R
+++ analysis.R
@@ -1,3 +1,13 @@
+update.packages(ask=FALSE, checkBuilt=TRUE)
+
 library(tidyverse)
 library(here)
+library(readr)
+library(janitor)
+library(skimr)
 
+data <- 
+  readr::read_csv("rgr_data_test (1).csv")
+
+summary(data)
+skim(data)
